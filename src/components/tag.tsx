5b973b4d21bfb99f9a0a5a344b078f85f04e267e
panchao5 <heydumboz@gmail.com>
2021-09-04 03:25:53 +0800

    rename variables

--- src/components/tag.tsx
+++ src/components/tag.tsx
@@ -12,14 +12,14 @@ const Tag = (props: TagProps) => (
   <span className={props.className}># {props.tag}</span>
 );
 
-const variants: Record<Variant, TwStyle> = {
-  filled: tw`text-white bg-gray-400 bg-opacity-70`,
-  outline: tw`text-current border-gray-400 border`,
+const styles: Record<Variant, TwStyle> = {
+  filled: tw`text-white bg-gray-500 bg-opacity-70`,
+  outline: tw`text-current border-gray-500 border`,
 };
 
 const StyledTag = styled(Tag)([
   tw`inline-block px-2.5 py-0.5 rounded-full text-sm`,
-  ({ variant = "filled" }) => variants[variant],
+  ({ variant = "filled" }) => styles[variant],
 ]);
 
 export default StyledTag;
